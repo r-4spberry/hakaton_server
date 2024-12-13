ccde7054f7ee79fe34aa2b5087d6c2ee70e7a559
Tomilov Daniil <todd@tavrida.com>
2024-12-13 11:37:42 +0300

    add CORS

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ from PIL import Image
 from pix2tex.cli import LatexOCR
 import tempfile
 from werkzeug.datastructures import FileStorage
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(
     app,
     version="1.0",
